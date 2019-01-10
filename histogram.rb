a702c1c35f4f4b9172a25cfa903ec12d4a7be8dd
arslan-tariq-confiz <46281746+arslan-tariq-confiz@users.noreply.github.com>
2019-01-10 15:28:27 +0500

    histogram.rb
    Histogram lab.

---
+++ histogram.rb
@@ -0,0 +1,19 @@
+puts "Enter text : "
+text = gets.chomp
+words = text.split(" ")
+
+frequencies = Hash.new(0)
+words.each {|word|
+  
+  frequencies[word]+=1  
+}
+
+frequencies =frequencies.sort_by {
+  |word, frequency|
+   frequency
+  }
+
+frequencies.reverse!
+frequencies.each {|word, frequency|
+  puts word + " " + frequency.to_s
+}
